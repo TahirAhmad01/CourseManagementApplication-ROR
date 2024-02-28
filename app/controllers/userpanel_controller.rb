5064b856c82e523c995c713da3aba4203be7c720
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-02-28 23:13:43 +0600

    fixed nav

--- app/controllers/userpanel_controller.rb
+++ app/controllers/userpanel_controller.rb
@@ -1,6 +1,6 @@
 class UserpanelController < ApplicationController
   def index
     @semesters = Semester.all
-    @courses  = Course.all
+    @courses  = Course.where(semester_id:current_user.semester_id)
   end
 end
