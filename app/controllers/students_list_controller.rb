f0f888f30972939a5efede2b58ed4a83f1444389
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-03-01 06:36:04 +0600

    add mark

--- app/controllers/students_list_controller.rb
+++ app/controllers/students_list_controller.rb
@@ -11,7 +11,32 @@ class StudentsListController < ApplicationController
   end
 
   def mark
+    @student_enrolled_courses = EnrolledCourse.where(users_id: params[:id])
+
+    if @student_enrolled_courses.empty?
+      flash[:error] = "User is not enrolled in any courses for the current semester."
+      redirect_to students_list_path(params[:id])
+      return
+    end
+
+    # Validation to check if marks are provided for all enrolled courses
+    # if params[:course_marks].nil? || params[:course_marks].values.any?(&:blank?)
+    #   flash[:error] = "Please provide marks for all enrolled courses."
+    #   redirect_to students_list_path(params[:id])
+    #   return
+    # end
+
+    # Iterate through each enrolled course
+    @student_enrolled_courses.each do |course|
+      marks = params[:course_marks]
+      # Create a mark record for the user, semester, and course
+      Mark.create(user_id: params[:id], semester_id: course.semester_id, course_id: course.course_id, marks: marks)
+    end
+
+    flash[:success] = "Marks given successfully."
+    redirect_to students_list_path(params[:id])
   end
+
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_084704) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_181330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_084704) do
     t.index ["semester_id"], name: "index_enrolled_courses_on_semester_id"
   end
 
+  create_table "marks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "semester_id", null: false
+    t.integer "marks"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "course_id", null: false
+    t.index ["course_id"], name: "index_marks_on_course_id"
+    t.index ["semester_id"], name: "index_marks_on_semester_id"
+    t.index ["user_id"], name: "index_marks_on_user_id"
+  end
+
   create_table "semesters", force: :cascade do |t|
     t.string "semester_name"
     t.datetime "created_at", null: false
@@ -77,5 +89,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_084704) do
   add_foreign_key "courses", "semesters"
   add_foreign_key "enrolled_courses", "courses"
   add_foreign_key "enrolled_courses", "semesters"
+  add_foreign_key "marks", "courses"
+  add_foreign_key "marks", "semesters"
+  add_foreign_key "marks", "users"
   add_foreign_key "users", "semesters"
 end
