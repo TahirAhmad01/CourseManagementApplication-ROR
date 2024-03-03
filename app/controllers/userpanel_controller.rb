a51f6390513563ab1db7a4d4d9346e71a44affa6
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-03-03 22:19:41 +0600

    fix design

--- app/controllers/userpanel_controller.rb
+++ app/controllers/userpanel_controller.rb
@@ -1,6 +1,7 @@
 class UserpanelController < ApplicationController
   def index
     @semesters = Semester.all
-    @courses  = Course.where(semester_id:current_user.semester_id)
+    @courses = Course.where(semester_id: current_user.semester_id)
+    @enrolled_courses = EnrolledCourse.where(semester_id: current_user.semester_id, users_id: current_user.id)
   end
 end
