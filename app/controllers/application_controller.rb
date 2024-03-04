a65d7cda602df2e130af467dbd6ec5bba2f9b31c
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-03-04 18:06:22 +0600

    fix result

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,4 @@
 # app/controllers/application_controller.rb
 
 class ApplicationController < ActionController::Base
-  helper_method :calculate_grade
-
-  def calculate_grade(score)
-    case score
-    when 80..100
-      'A+'
-    when 75..79
-      'A'
-    when 70..74
-      'A-'
-    when 65..69
-      'B+'
-    when 60..64
-      'B'
-    when 55..59
-      'B-'
-    when 50..54
-      'C+'
-    when 45..49
-      'C'
-    when 40..44
-      'D'
-    else
-      'F'
-    end
-  end
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,31 +2,83 @@ module ApplicationHelper
   def calculate_cgpa(enrolled_courses)
     total_credit_points = 0
     total_credits = 0
+    failed_courses = []
 
     enrolled_courses.each do |enrolled_course|
       next if enrolled_course.marks.nil?
 
-      grade = calculate_grade(enrolled_course.marks)
       credit = enrolled_course.course.credit
+      grade_points = calculate_grade(enrolled_course.marks)
 
-      case grade
-      when 'A+' then gp = 4.00
-      when 'A' then gp = 3.75
-      when 'A-' then gp = 3.50
-      when 'B+' then gp = 3.25
-      when 'B' then gp = 3.00
-      when 'B-' then gp = 2.75
-      when 'C+' then gp = 2.50
-      when 'C' then gp = 2.25
-      when 'D' then gp = 2.00
-      else gp = 0.00
+      if grade_points == 0
+        total_credit_points = 0
+        break
+      else
+        total_credit_points += grade_points * credit
+        total_credits += credit
       end
-
-      total_credit_points += gp * credit
-      total_credits += credit
     end
 
-    total_credits.zero? ? 0 : total_credit_points / total_credits
+    failed_courses.each do |course_name|
+      puts "You have failed in #{course_name}"
+    end
+
+    total_credits.zero? ? "" : total_credit_points / total_credits
+  end
+
+  def grade_alpha(cgpa)
+    case cgpa
+    when 3.75..4.00
+      'A+'
+    when 3.50..3.74
+      'A'
+    when 3.25..3.49
+      'A-'
+    when 3.00..3.24
+      'B+'
+    when 2.75..2.99
+      'B'
+    when 2.50..2.74
+      'B-'
+    when 2.25..2.49
+      'C+'
+    when 2.00..2.24
+      'C'
+    when 1.75..1.99
+      'C-'
+    when 1.50..1.74
+      'D+'
+    when 1.00..1.49
+      'D'
+    else
+      'F'
+    end
+  end
+
+  private
+
+  def calculate_grade(score)
+    case score
+    when 80..100
+      4.00
+    when 75..79
+      3.75
+    when 70..74
+      3.50
+    when 65..69
+      3.25
+    when 60..64
+      3.00
+    when 55..59
+      2.75
+    when 50..54
+      2.50
+    when 45..49
+      2.25
+    when 40..44
+      2.00
+    else
+      0.00
+    end
   end
 end
-
