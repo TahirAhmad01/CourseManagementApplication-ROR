5522a3214b73f8d39467c2582ceef0a0a20f0624
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-03-06 15:53:43 +0600

    fixed total cgpa calculate

--- app/helpers/result_helper.rb
+++ app/helpers/result_helper.rb
@@ -5,6 +5,7 @@ module ResultHelper
     semesters = Semester.all
     total_gpa = 0.0
     total_credit_hours = 0
+    enrolled_courses_exits = false
 
     all_semesters_exist = semesters.pluck(:id).all? { |semester_id| unique_semesters.pluck(:semester_id).include?(semester_id) }
 
@@ -13,12 +14,20 @@ module ResultHelper
         if enrolled_course.marks.present?
           total_gpa += calculate_grade(enrolled_course.marks) * enrolled_course.course.credit
           total_credit_hours += enrolled_course.course.credit
+          enrolled_courses_exits = true
+        else
+          enrolled_courses_exits = false
+          break
         end
       end
 
-      total_cgpa = calculate_cgpa( enrolled_courses)
-      Rails.logger.debug("Total CGPA: #{total_cgpa}")
-      return total_cgpa
+      if enrolled_courses_exits
+        total_cgpa = calculate_cgpa( enrolled_courses)
+        return total_cgpa
+      else
+        return "result is pending"
+      end
+
     else
       Rails.logger.debug("Not all semester IDs exist in unique semesters.")
       return "result is pending"
--- app/helpers/students_list_helper.rb
+++ app/helpers/students_list_helper.rb
@@ -1,10 +1,12 @@
 module StudentsListHelper
+
   def total_result
     enrolled_courses = EnrolledCourse.where(users_id: params[:id])
     unique_semesters = enrolled_courses.select(:semester_id).distinct
     semesters = Semester.all
     total_gpa = 0.0
     total_credit_hours = 0
+    enrolled_courses_exits = false
 
     all_semesters_exist = semesters.pluck(:id).all? { |semester_id| unique_semesters.pluck(:semester_id).include?(semester_id) }
 
@@ -13,15 +15,23 @@ module StudentsListHelper
         if enrolled_course.marks.present?
           total_gpa += calculate_grade(enrolled_course.marks) * enrolled_course.course.credit
           total_credit_hours += enrolled_course.course.credit
+          enrolled_courses_exits = true
+        else
+          enrolled_courses_exits = false
+          break
         end
       end
 
-      total_cgpa = calculate_cgpa( enrolled_courses)
-      Rails.logger.debug("Total CGPA: #{total_cgpa}")
-      total_cgpa
+      if enrolled_courses_exits
+        total_cgpa = calculate_cgpa( enrolled_courses)
+        return total_cgpa
+      else
+        return "result is pending"
+      end
+
     else
       Rails.logger.debug("Not all semester IDs exist in unique semesters.")
-      "result is pending"
+      return "result is pending"
     end
   end
 end
