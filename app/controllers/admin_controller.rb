c91d7f2298e7dd74bc38740c198c29019f921fc0
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-03-07 17:21:36 +0600

    fix admin home paage design

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,8 @@
 class AdminController < ApplicationController
   def index
+    @total_student= User.all.count
+    @total_semester= Semester.all.count
+    @total_courses = Course.all.count
+    @total_graduated_students = User.where.not(cgpa: nil).count
   end
 end
